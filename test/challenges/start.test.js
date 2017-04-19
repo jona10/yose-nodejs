b42e47f84832bbbf35a238e00254be49d345b735
Jonathan Falardeau <jonathan.falardeau@gmail.com>
2017-04-18 22:03:46 -0400

    Improve test readability by using CheerioJS.

--- test/challenges/start.test.js
+++ test/challenges/start.test.js
@@ -1,8 +1,13 @@
 'use strict'
 
-const expect = require('chai').expect
+const chai = require('chai')
+const cheerio = require('cheerio')
+const chaiCheerio = require('chai-cheerio')
 const fetch = require('node-fetch')
 const Yose = require('../../src/yose')
+
+const expect = chai.expect
+chai.use(chaiCheerio)
 
 describe('The game', () => {
   let server
@@ -17,8 +22,11 @@ describe('The game', () => {
   })
 
   it('should greet', () => {
-    return fetch('http://127.0.0.1:9000/').then(response => response.text()).then(body => {
-      expect(body).to.contain('Hello Yose')
+    return fetch('http://127.0.0.1:9000/').then(response => cheerio.load(response.text())).then($ => {
+      let greeting = $('h1')
+
+      expect(greeting).to.exist
+      expect(greeting).to.have.text('Hello Yose')
     })
   })
 
@@ -29,8 +37,13 @@ describe('The game', () => {
   })
 
   it('should link to repository', () => {
-    return fetch('http://127.0.0.1:9000/').then(response => response.text()).then(body => {
-      expect(body).to.contain('<a id="repository-link" href="https://github.com/jona10/yose-nodejs" target="_blank">Github</a>')
+    return fetch('http://127.0.0.1:9000/').then(response => cheerio.load(response.text())).then($ => {
+      let link = $('a#repository-link')
+
+      expect(link).to.exist
+      expect(link).to.have.prop('href', 'https://github.com/jona10/yose-nodejs')
+      expect(link).to.have.prop('target', '_blank')
+      expect(link).to.have.text('Github')
     })
   })
 })
