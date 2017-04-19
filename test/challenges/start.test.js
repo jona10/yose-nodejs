c8f28480ca07fff78832bfe08be9d533e11cd8f5
Jonathan Falardeau <jonathan.falardeau@gmail.com>
2017-04-18 22:18:30 -0400

    Move to request instead of node-fetch.

--- test/challenges/start.test.js
+++ test/challenges/start.test.js
@@ -3,13 +3,14 @@
 const chai = require('chai')
 const cheerio = require('cheerio')
 const chaiCheerio = require('chai-cheerio')
-const fetch = require('node-fetch')
+const request = require('request-promise')
 const Yose = require('../../src/yose')
 
 const expect = chai.expect
 chai.use(chaiCheerio)
 
 describe('The game', () => {
+  let baseUri = 'http://127.0.0.1:9000'
   let server
 
   before(() => {
@@ -21,29 +22,41 @@ describe('The game', () => {
     server.stop()
   })
 
-  it('should greet', () => {
-    return fetch('http://127.0.0.1:9000/').then(response => cheerio.load(response.text())).then($ => {
-      let greeting = $('h1')
+  describe('when querying the home page', () => {
+    let options = {
+      uri: baseUri + '/',
+      transform: cheerio.load,
+    }
 
-      expect(greeting).to.exist
-      expect(greeting).to.have.text('Hello Yose')
+    it('should greet', () => {
+      return request(options).then($ => {
+        let greeting = $('h1')
+
+        expect(greeting).to.exist
+        expect(greeting).to.have.text('Hello Yose')
+      })
+    })
+
+    it('should link to repository', () => {
+      return request(options).then($ => {
+        let link = $('a#repository-link')
+
+        expect(link).to.exist
+        expect(link).to.have.prop('href', 'https://github.com/jona10/yose-nodejs')
+        expect(link).to.have.prop('target', '_blank')
+        expect(link).to.have.text('Github')
+      })
     })
   })
 
   it('should be alive', () => {
-    return fetch('http://127.0.0.1:9000/ping').then(response => response.json()).then(json => {
+    let options = {
+      uri: baseUri + '/ping',
+      json: true,
+    }
+
+    return request(options).then(json => {
       expect(json).to.deep.equal({alive: true})
-    })
-  })
-
-  it('should link to repository', () => {
-    return fetch('http://127.0.0.1:9000/').then(response => cheerio.load(response.text())).then($ => {
-      let link = $('a#repository-link')
-
-      expect(link).to.exist
-      expect(link).to.have.prop('href', 'https://github.com/jona10/yose-nodejs')
-      expect(link).to.have.prop('target', '_blank')
-      expect(link).to.have.text('Github')
     })
   })
 })
